10048f782b9608062c13a8e4b534fe97791fd8f5
GRAMI SALMA <salmagrami0@gmail.com>
2024-03-23 20:35:53 +0000

    cacher le mot de passe

--- src/main/java/ma/emsi/exemplerolesjee/entities/User.java
+++ src/main/java/ma/emsi/exemplerolesjee/entities/User.java
@@ -1,5 +1,6 @@
 package ma.emsi.exemplerolesjee.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class User {
 
     @Column(name="USER_NAME",unique=true,length=20)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     @ManyToMany(mappedBy ="users",fetch= FetchType.EAGER)
     private List<Role> roles=new ArrayList<>();
